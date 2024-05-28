9ce7d9d1cb14ff36814a8c9dc3ecd48bec6cd432
Lucas <lucas.carrijoferrari@gmail.com>
2024-05-28 11:42:33 -0300

    feat: estrutura básica (texto, imagem, texto)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,39 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+    val titulo by remember { mutableStateOf("Wicked") }
+    val nota by remember { mutableStateOf("Nota: 9.2 - Ano: 2024") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Text(text = titulo)
+            Image(
+                painter = painterResource("capaFilme.jpg"),
+                contentDescription  = "Capa Wicked",
+                modifier = Modifier.width(200.dp)
+            )
+            Text(text = nota)
         }
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication, title = "7DaysOfCode") {
         App()
     }
 }
