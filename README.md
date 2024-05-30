4c3acbe33701e07d77a1a1783e78ed72981cbf68
luwucaz <lucas.carrijoferrari@gmail.com>
2024-05-30 11:58:22 -0300

    feat: novo layout e cores

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # 7DaysOfCode
 
 ## Screenshots
+
 ### Day 1
+
 ![Day 1](src/main/resources/capturasDeTelaDias/dia1.png)
+
 ### Day 2
+
 ![Day 2](src/main/resources/capturasDeTelaDias/dia2.png)
 
+### Day 3
+
+![Day 3](src/main/resources/capturasDeTelaDias/dia3.png)
+
 ## Thanks
+
 - https://github.com/succlz123/compose-desktop-imageloader/
 
 ## Disclaimer
-This project is for educational purposes only. It is not intended to be a commercial product and no profit is being made from its development.
+
+This project is for educational purposes only. It is not intended to be a commercial product and no profit is being made
+from its development.
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,26 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import org.succlz123.lib.imageloader.ImageAsyncImageUrl
@@ -18,39 +31,82 @@ import org.succlz123.lib.imageloader.core.ImageCallback
 @Preview
 fun App() {
     val titulo by remember { mutableStateOf("Wicked") }
-    val nota by remember { mutableStateOf("Nota: 9.2 - Ano: 2024") }
+    val nota by remember { mutableStateOf("9.2") }
+    val ano by remember { mutableStateOf("2024") }
+
+    val tamanhoFonte = 15
+    val tamanhoFonteTitulo = 12
 
     MaterialTheme {
-        Column {
-            Text(text = titulo)
-            ImageAsyncImageUrl("https://i.imgur.com/g1n1j6D.jpeg",
-                imageCallback = ImageCallback {
-                    Image(
-                        modifier = Modifier.width(200.dp),
-                        painter = it,
-                        contentDescription = "123")
-                })
-            Text(text = nota)
+        Surface(
+            //color = Color(0xFF140c0c),
+            color = Color(0xFF121212),
+            shape = RoundedCornerShape(5.dp),
+            modifier = Modifier.size(width = 200.dp, height = 350.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(10.dp)
+            ) {
+                Column {
+                    ImageAsyncImageUrl("https://i.imgur.com/g1n1j6D.jpeg",
+                        imageCallback = ImageCallback {
+                            Image(
+                                modifier = Modifier
+                                    .width(200.dp)
+                                    .clip(shape = RoundedCornerShape(5.dp)),
+                                painter = it,
+                                contentDescription = "Capa Wicked"
+                            )
+                        })
+                    Column {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 5.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+
+                            ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(
+                                    Icons.Rounded.Star,
+                                    contentDescription = "Estrela",
+                                    modifier = Modifier
+                                        .size(tamanhoFonte.dp),
+                                    tint = Color.Yellow
+                                )
+                                Text(
+                                    text = nota,
+                                    color = Color.White,
+                                    fontSize = tamanhoFonte.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                            Text(text = ano, color = Color.White, fontSize = tamanhoFonte.sp)
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = titulo,
+                                color = Color.White,
+                                textAlign = TextAlign.Center,
+                                fontSize = tamanhoFonteTitulo.sp
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
-//fun requestHttp(link: String): String {
-//    val client: HttpClient = HttpClient.newHttpClient()
-//    val request = HttpRequest.newBuilder()
-//        .uri(URI.create(link))
-//        .build()
-//    client.sendAsync(request, BodyHandlers.ofString())
-//        .thenApply { obj: HttpResponse<*> -> obj.body() }
-//        .thenAccept(System.out::println)
-//        .join()
-//
-//    val response = client
-//        .send(request, BodyHandlers.ofString())
-//
-//    println(response.body())
-//    return response.body()
-//}
 
 fun main() = application {
     Window(
